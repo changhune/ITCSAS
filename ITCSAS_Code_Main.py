8be29e25767b04e056816a4b2eb9da643c11ccf2
pjcarriere <49417278+pjcarriere@users.noreply.github.com>
2019-04-15 15:34:51 -0400

    Add files via upload

--- ITCSAS_Code_Main.py
+++ ITCSAS_Code_Main.py
@@ -5,36 +5,53 @@ import busio
 import digitalio
 import adafruit_max31855
 import adafruit_character_lcd.character_lcd_rgb_i2c as character_lcd
+import ITCSAS_Config as cfg
+
 # Define Pins For Thermocouple Amplifier Communication
 spi = busio.SPI(board.SCK, MOSI=board.MOSI, MISO=board.MISO)
 cs = digitalio.DigitalInOut(board.D5)
-max31855 = adafruit_max31855.MAX31855(spi, cs)
+amp = adafruit_max31855.MAX31855(spi, cs)
+
 # Define LCD Row/Column Numbers -- 2 Rows of 16 Columns Each
 lcd_columns = 16
 lcd_rows = 2
+
 # Initialise I2C bus
 i2c = busio.I2C(board.SCL, board.SDA)
+
 # Initialise the LCD class
 lcd = character_lcd.Character_LCD_RGB_I2C(i2c, lcd_columns, lcd_rows)
 
 # Clear text from LCD
 lcd.clear()
 
-while True:
-	# Read Temperature from Amplifier
-	tempC = max31855.temperature
-	
-	# Convert Temperature to Fahreneit
-    #tempF = tempC * 9 / 5 + 32
-	
-	# Optional: Print temperature to terminal
-    #print('Temperature: {} C {} F '.format(tempC, tempF))
-	
-	lcd.color = [255,255,255]
-	lcdText = ("Temp.:"+tempC+"°C\n"+"LINE 2")
-	lcd.message = lcdText
-	
-	# Wait 2 Seconds
-    time.sleep(2)
-		
+while True:  
+	# Take about 30 seconds to take 2 temperature readings 15 seconds apart and average them.
+	tempSum = 0
+	for x in range(numAvg+1):
+		tempSum = tempSum+amp.temperature
+		time.sleep(cfg.timeAvg)  # Wait between readings
+	tempAvg = tempSum/numAvg  # Calculate average
 
+	tempC = tempAvg+cfg.tempOffset  # Adjust temperature based on value defined in config
+
+	# Convert Temperature from Celsius to Fahreneit
+    tempF = tempC*9/5+32
+	
+	# Convert Temperature from Celsius to Kelvin
+	tempK = tempC+273.15
+	
+	# Optional: Print Temperature to Terminal (Uncomment Next Line)
+    #print('Temperature: {} C {} F {} K'.format(tempC, tempF, tempK))
+	
+	if cfg.dispTempFormat == 1:
+		dispTemp = round(tempF,1)
+	else:
+		if cfg.dispTempFormat == 2:
+			dispTemp = round(tempK,1)
+		else:
+			dispTemp = round(tempC,1)
+			
+	lcd.color = cfg.defaultDisplayColor
+	lcdText = ("Temp.: "+dispTemp+"°C\n"+"LINE 2")
+	lcd.message = lcdText
---
+++ ITCSAS_Config.py
@@ -0,0 +1,21 @@
+######################################################################
+# IMPROVED THERMAL CONCRETE STRENGTH ASSESSMENT SYSTEM CONFIGURATION #
+######################################################################
+
+# LCD/Display Settings
+
+	# Set RGB Display Color 
+	dispColor = [255,255,255]  # [RED,GREEN,BLUE] (0-255)
+	
+	# Set Temperature Format - Celsius, Fahrenheit, or Kelvin
+	dispTempFormat = 0  # 0 for °C, 1 for °F, 2 for K
+	
+# Temperature Reading Settings
+
+	# Set Number of Readings to Average
+	numAvg = 2
+	# Set Time Between Readings
+	timeAvg = 15
+	# Set Temperature Adjustment Offset
+	tempOffset = 0
+	
